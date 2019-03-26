8b487fa792809328395e25116ac9e3f83a3f1b8a
Ryan W <w.ryan@stelch.com>
2019-03-27 00:38:48 +1100

    Core updates

--- index.php
+++ index.php
@@ -26,30 +26,58 @@
 
         <div class="applications">
             <div class="header">
-                <p class="label">Welcome to your Applications homepage!</p>
+                <!--<p class="label">Welcome to your Applications homepage!</p>-->
             </div>
 
-            <div class="content-holder">
+            <div id="my-app-container" class="content-holder">
                 <div class="title">
                     <span class="left">My Applications</span>
-                    <span class="right"><i class="fa fa-caret-down"></i></span>
-                </div><br/>
+                    <span class="right">
+                        <i class="fa fa-caret-right"></i>
+                        <i class="fa fa-caret-down"></i>
+                    </span>
+                </div>
                 <div class="content">
                     You have no open applications
                 </div>
             </div>
 
-            <div class="content-holder">
+            <div id="opn-app-container" class="content-holder">
                 <div class="title">
                     <span class="left">Open Applications</span>
-                    <span class="right"><i class="fa fa-caret-down"></i></span>
-                </div><br/>
+                    <span class="right">
+                        <i class="fa fa-caret-right"></i>
+                        <i class="fa fa-caret-down"></i>
+                    </span>
+                </div>
                 <div class="content">
-                    There are no open Applications at this time.
+                    <div class="application-box">
+                        <div class="title">FakeJake Moderator Application</div>
+                        <div class="title">Do you have what it takes to represent FakeJake's Discord server as a Moderator? Why not try out for the position?</div>
+                        <div class="closes">
+                            <p>Closes</p>
+                            <p>30/03/2019</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
         </div>
 
     </div>
+
+<div class="watermark">
+    Software by <i>Stelch</i>
+</div>
 </body>
+
+<script>
+    document.getElementById("opn-app-container").onmousedown=(e)=>{
+        e.preventDefault();
+        document.getElementById("opn-app-container").classList.toggle("active");
+    }
+    document.getElementById("my-app-container").onmousedown=(e)=>{
+        e.preventDefault();
+        document.getElementById("my-app-container").classList.toggle("active");
+    }
+</script>
